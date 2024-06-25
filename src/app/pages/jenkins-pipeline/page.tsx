59ef09287ac418810ecd3ab4147171002d2087a0
pttiwari11 <pushpendra.tiwari_cs19@gla.ac.in>
2024-06-25 22:18:08 +0530

    ui change vercel deploy test

---
+++ src/app/pages/jenkins-pipeline/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+            Build 6 successfull!!!
+        </div>
+    )
+}
---
+++ src/app/pages/vercel/page.tsx
@@ -0,0 +1,7 @@
+import DefaultPage from "@/app/components/DefaultPage"
+
+export default function Page() {
+    return (
+        <DefaultPage />
+    )
+}
